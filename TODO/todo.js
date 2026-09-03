414cfded69e1c095d90fb1d8bc35c77badacebc0
Kowsar096 <kowsarwebdesigner@gmail.com>
2026-09-04 00:36:07 +0600

    Fix task rendering and button functionality in Todo app

--- TODO/todo.js
+++ TODO/todo.js
@@ -16,17 +16,24 @@ function render(){
         let span = document.createElement('span');
 
         span.classList.add('task-text');
-        span.textContent = task.text;
+        span.textContent = `task:${task.text} completed: ${task.completed}`;
 
         let btnWrapper = document.createElement('div');
-        btnWrapper.classList.add('task-button');
+        btnWrapper.classList.add('task-buttons');
 
         let completeBtn = document.createElement('button');
 
-        completeBtn.textContent = '✅'
+        completeBtn.textContent = '✅';
+        completeBtn.addEventListener('click', ()=>{
+            completeTask(task.id);
+        })
 
         let deleteBtn = document.createElement('button');
         deleteBtn.textContent = '❌';
+
+        deleteBtn.addEventListener('click', ()=>{
+            deleteTask(task.id);
+        })
 
         btnWrapper.appendChild(completeBtn);
         btnWrapper.appendChild(deleteBtn);
@@ -51,8 +58,8 @@ function addTask(){
         text: taskText,
         completed: false,
     };
-
-    task.push(newTask);
+    tasks.push(newTask);
+    // task.push(newTask);
     taskInput.value = '';
     render()
 
